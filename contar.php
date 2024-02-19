03c843a9b5da846e14d86d3274ac6e706cb96416
JhotaCruz <jhon_tsc@hotmail.com>
2024-02-19 12:03:50 -0500

    cambios

---
+++ contar.php
@@ -0,0 +1,58 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Definición
+$aNotas = array(9, 8, 9.50, 4, 7, 8);
+
+$aPacientes = array();
+$aPacientes[] = array(
+    "dni" => "33.765.012",
+    "nombre" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 81.50,
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79,
+);
+$aPacientes[] = array(
+    "dni" => "11.568.778",
+    "nombre" => "Martín Perez",
+    "edad" => 26,
+    "peso" => 77,
+);
+
+$aProductos = array();
+$aProductos[] = array("nombre" => "Smart TV 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000,
+);
+$aProductos[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000,
+);
+
+
+
+function contar($aArray){
+    $cont = 0;
+    foreach($aArray as $item){
+        $cont++;
+    }
+    return $cont;
+}
+
+//Uso
+echo "Cantidad de productos: " . contar($aProductos) . "<br>";
+echo "Cantidad de pacientes: " . contar($aPacientes) . "<br>";
+echo "Cantidad de notas: " . contar($aNotas);
+
+?>
