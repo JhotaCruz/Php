0069e80b26be06237f27ace8899884e8462dabaf
JhotaCruz <jhon_tsc@hotmail.com>
2024-02-20 09:53:41 -0500

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -3,7 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function promediar($aNumeros){
+function promediar($aNumeros)
+{
     $sumatoria = 0;
     foreach ($aNumeros as $numero) {
         $sumatoria += $numero; //$sumatoria = $sumatoria + $numero;
