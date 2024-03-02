0f24657472faf48f6104c0cf677cdc63833824ba
JhotaCruz <jhon_tsc@hotmail.com>
2024-03-02 12:19:54 -0500

    cambio

--- sistema_ventas/config.php
+++ sistema_ventas/config.php
@@ -6,14 +6,14 @@ error_reporting(E_ALL);
 //Iniciamos la session
 session_start();
 
-//date_default_timezone_set('America/Argentina/Buenos_Aires');
-date_default_timezone_set("America/Bogota");
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+//date_default_timezone_set("America/Bogota");
 
 
 class Config {
-    const BBDD_HOST = "127.0.0.1"; //127.0.0.1
-    const BBDD_PORT = "3306"; //3306
-    const BBDD_USUARIO = "root"; //root
+    const BBDD_HOST = "127.0.0.1"; 
+    const BBDD_PORT = "3306"; 
+    const BBDD_USUARIO = "root"; 
     const BBDD_CLAVE = "";
     const BBDD_NOMBRE = "abmventas";
 
