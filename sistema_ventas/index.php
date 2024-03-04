488b8fbbe387709191cca77419248a249b9c62a5
JhotaCruz <jhon_tsc@hotmail.com>
2024-03-04 16:05:34 -0500

    cambio

--- sistema_ventas/index.php
+++ sistema_ventas/index.php
@@ -1,9 +1,16 @@
 <?php
 
-include_once("config.php");
-include_once("entidades/venta.php");
-
+include_once "config.php";
+include_once "entidades/venta.php";
 $pg = "Inicio";
+
+$venta = new Venta();
+$facturacionMes = $venta->obtenerFacturacionMensual(date("m"), date("Y"));
+
+$fechaHasta = date("Y-m-d");
+$date = new DateTime($fechaHasta);
+$fechaDesde = date_format($date->modify("-12 months"), "Y-m-d");
+$facturacionAnual = $venta->obtenerFacturacionPorPeriodo($fechaDesde, $fechaHasta);
 
 include_once("header.php"); 
 
@@ -28,7 +35,7 @@ include_once("header.php");
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Facturación (mensual)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$ </div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$ <?php echo number_format($facturacionMes, 2, ",", "."); ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -45,7 +52,7 @@ include_once("header.php");
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Facturación (anual)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$ <?php echo number_format($facturacionAnual, 2, ",", ".");?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
